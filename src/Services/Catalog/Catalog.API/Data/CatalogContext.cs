fa2d999a1f88fab6edd130abf7c97262db7b1535
Aaron <huynhtruongdyu@gmail.com>
2023-10-06 01:45:36 +0700

    Add project files.

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,45 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Settings;
+
+using Microsoft.Extensions.Options;
+
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data;
+
+public interface ICatalogContext
+{
+    IMongoCollection<Product> Products { get; }
+}
+
+public class CatalogContext : ICatalogContext
+{
+    public CatalogContext(IOptions<DatabaseSettings> databaseSettings)
+    {
+        var mongoClient = new MongoClient(databaseSettings.Value.ConnectionString);
+        var database = mongoClient.GetDatabase(databaseSettings.Value.DatabaseName);
+        // Check if the database exists, create it if not
+        //{
+        //    var databaseNames = mongoClient.ListDatabaseNames().ToList();
+        //    if (!databaseNames.Contains(databaseSettings.Value.DatabaseName))
+        //    {
+        //        string createJson = "{create: " + databaseSettings.Value.DatabaseName + "}";
+        //        var createDatabseCommand = new JsonCommand<BsonDocument>(createJson);
+        //        mongoClient.GetDatabase("admin").RunCommand(createDatabseCommand);
+        //    }
+        //}
+        Products = database.GetCollection<Product>(databaseSettings.Value.CollectionName);
+        // Check if the collection exists, create it if not
+        //{
+        //    var collectionNames = database.ListCollectionNames().ToList();
+        //    if (!collectionNames.Contains(databaseSettings.Value.CollectionName))
+        //    {
+        //        database.CreateCollection(databaseSettings.Value.CollectionName);
+        //    }
+        //}
+        CatalogContextSeed.SeedData(Products);
+    }
+
+    public IMongoCollection<Product> Products { get; }
+}
